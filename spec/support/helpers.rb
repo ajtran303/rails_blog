538c0d9f5af912e40a86daab7e052e1a25762c17
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2020-11-25 20:09:29 -0700

    Configure Helper module for RSpec tests

---
+++ spec/support/helpers.rb
@@ -0,0 +1,19 @@
+module Helpers
+  def page_does thing
+    expect(page).to thing
+  end
+
+  def page_has_link link
+    page_does have_link *link
+  end
+
+  def page_has_text text
+    page_does have_text text
+  end
+
+  def page_has_links links
+    links.each do |link|
+      page_has_link link
+    end
+  end
+end
